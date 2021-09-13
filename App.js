7cfdab38adb0c4aea20e3c2ec9e9475690984f36
Girish Hari <bunnefant@gmail.com>
2021-09-12 23:29:32 -0400

    fixed merge conflict

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { NativeRouter, Switch, Route } from 'react-router-native';
 import PublicRoute from './routes/PublicRoute';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-<<<<<<< HEAD
+
 import BlogView from './components/BlogView';
 import PostView from './components/PostView';
-=======
+
 import AuthProvider from './contexts/AuthContext';
->>>>>>> e734086f6992b950a740f8548fbba19d2fb12094
+
 
 
 
@@ -19,15 +19,11 @@ export default function App() {
     <AuthProvider>
       <NativeRouter>
         <Switch>
-<<<<<<< HEAD
+
           <Route exact path='/' component={LoginForm}/>
           <Route exact path='/register' component={RegisterForm}/>
           <Route exact path='/home' component={BlogView}/>
           <Route exact path='/post' component={PostView}/>
-=======
-          <Route exact path='/login' component={LoginForm} />
-          <Route exact path='/register' component={RegisterForm} />
->>>>>>> e734086f6992b950a740f8548fbba19d2fb12094
         </Switch>
       </NativeRouter>
     </AuthProvider>
